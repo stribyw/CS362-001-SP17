138daba4c1a517897de04cd064ab2efd5070261b
stribyw <stribyw@oregonstate.edu>
2017-04-15 11:55:57 -0700

    modified arrayManip.java after realizing that not all functions were being called.

--- projects/stribyw/assignment-1/hw1/src/main/java/edu/osu/cs362/arrayManip.java
+++ projects/stribyw/assignment-1/hw1/src/main/java/edu/osu/cs362/arrayManip.java
@@ -13,14 +13,17 @@ public class arrayManip
 		findAndPrintPairs(list, 5);
 		bubblesort(list);
 		showList(list);
+		findMin(list);
 
 		list = new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 		bubblesort(list);
 		showList(list);
+		badResize(list, 20);
 
 		list = new int[]{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2};
 		bubblesort(list);
 		showList(list);
+		goodResize(list, 22);
 
 		list = new int[]{1};
 		bubblesort(list);
